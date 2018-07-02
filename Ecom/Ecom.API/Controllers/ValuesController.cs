32a6a42ddd039f8208dc27200232d55765615505
Velibor Stancic <v.stancic@vegaitsourcing.rs>
2018-07-02 17:41:55 +0200

    Finshed logger

--- Ecom/Ecom.API/Controllers/ValuesController.cs
+++ Ecom/Ecom.API/Controllers/ValuesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ecom.LoggerService.Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ecom.API.Controllers
@@ -9,10 +10,21 @@ namespace Ecom.API.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private ILoggerManager _logger;
+
+        public ValuesController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
+            _logger.LogInfo("Here is info message from our values controller.");
+            _logger.LogDebug("Here is debug message from our values controller.");
+            _logger.LogWarn("Here is warn message from our values controller.");
+            _logger.LogError("Here is error message from our values controller.");
+
             return new string[] { "value1", "value2" };
         }
 
---
+++ Ecom/Ecom.API/Exstensions/ServiceExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Ecom.LoggerService;
+using Ecom.LoggerService.Contracts;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecom.API.Exstensions
+{
+    public static class ServiceExtensions
+    {
+
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
+
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+        }
+    }
+}
