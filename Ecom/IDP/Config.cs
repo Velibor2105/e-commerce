a7b9bf8e90020d37c34e8aec9be3d7e4c79a462f
Velibor Stancic <v.stancic@vegaitsourcing.rs>
2018-07-09 17:25:49 +0200

    added oidc

---
+++ Ecom/IDP/Config.cs
@@ -0,0 +1,89 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace IDP
+{
+    public static class Config
+    {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "fec0a4d6-5830-4eb8-8024-272bd5d6d2bb",
+                    Username = "Velibor",
+                    Password = "Velibor",
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Velibor"),
+                        new Claim("family_name", "Stancic"),
+                        new Claim("role", "Administrator"),
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "c3b7f625-c07f-4d7d-9be1-ddff8ff93b4d",
+                    Username = "Ana",
+                    Password = "Ana",
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Ana"),
+                        new Claim("family_name", "Tot"),
+                        new Claim("role", "Seler"),
+                    }
+                }
+            };
+        }
+
+        public static List<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+               new IdentityResources.OpenId(),
+               new IdentityResources.Profile(),
+               new IdentityResource("roles", "Your role(s)", new []{"role"}),
+            };
+        }
+
+        internal static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new[] {
+                new ApiResource("ecomapi", "Ecom Management API", new[] { "role" })
+            };
+        }
+
+        public static List<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientName = "Ecom Management",
+                    ClientId="ecomangularclient",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    RequireConsent = false,
+                    AllowAccessTokensViaBrowser = true,
+                    RedirectUris =new List<string>
+                    {
+                        "https://localhost:4200/signin-oidc"
+                    },
+                    AccessTokenLifetime = 180,
+                    PostLogoutRedirectUris = new[]{
+                        "https://localhost:4200/"
+                    },
+                    AllowedScopes = new []
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "roles",
+                        "ecomapi",
+                    }
+                }
+            };
+        }
+    }
+}
